e1b53128adef2259000c9d38639d8ce638c5d232
pavel_rum <p.rumyancev@involta.ru>
2022-05-08 23:49:49 +0300

    first commit

---
+++ src/components/ChatSidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sidebar">
+    <p class="sidebar__logo">CO. LOGO</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <form class="form">
+      <p class="form__title">{{ title }}</p>
+      <div class="form__input-wrapper">
+        <input type="text" class="form__input" name="login" :placeholder="$t('form.username')">
+        <input type="password" class="form__input" name="password" :placeholder="$t('form.password')">
+      </div>
+      <input type="submit" class="form__submit" :disabled="disabled" :value="$t('form.login')">
+      <router-link class="form__link" to="/registration">{{ $t("form.registration") }}</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      title: '',
+      disabled: true
+    }
+  },
+  mounted() {
+    this.title = process.env.VUE_APP_TITLE
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
